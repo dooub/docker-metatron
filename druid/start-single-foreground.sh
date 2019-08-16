ecd732c81f4523b899ffa185162401dc982ca0e8
Will Choe <will@dooub.com>
2019-08-16 19:06:02 +0900

    docker metatrons

--- druid/start-single-foreground.sh
+++ druid/start-single-foreground.sh
@@ -1,3 +1,11 @@
 #!/bin/bash -eu
 
+export METATRON_CONFIG_DIRECTORY=conf
+export METATRON_CONFIG_DIRECTORY_ORIGIN=conf_origin
+
+if [[ ! -e ${METATRON_CONFIG_DIRECTORY}/druid ]]; then
+    mkdir ${METATRON_CONFIG_DIRECTORY}
+    cp -R conf_origin/* conf/
+fi
+
 java `cat conf/druid/single/jvm.config | xargs` -cp conf/druid/_common:conf/druid:conf/druid/single:lib/*:/usr/local/Cellar/hadoop/2.7.2/libexec/etc/hadoop io.druid.cli.ServerRunnable $@
